0d66a79f9d904264f4c1313954060598aa3f19eb
barnest3486-oss <barnest3486@student.faytechcc.edu>
2026-09-20 23:46:55 -0400

    first Input assignment
    Making an input and printing the output.

---
+++ P1LAB1_BarnesTerrence.py
@@ -0,0 +1,9 @@
+# Terrence Barnes Jr.
+# 9/20/2026
+# P1LAB1.py
+# input and print functions
+
+first_name = input("Enter your first name")
+last_name = input("Enter your last name")
+
+print ("Hello,",first_name,last_name +"!","Welcome to CTI-110")
